c3ab0c0bd87e90dffa6d6d613d5a659d15734054
AsaNeedle <asa@cottageclass.com>
2020-03-04 14:03:25 -0500

    Updates

---
+++ precache-manifest.b8708c38893f6e535d4b1dca95967458.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0522857a15fffad1ac0946faab3372cc",
+    "url": "/trello-style-app/index.html"
+  },
+  {
+    "revision": "786b254f13638e4672e8",
+    "url": "/trello-style-app/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "a27ceb1f73f865f90f8a",
+    "url": "/trello-style-app/static/css/main.b59e25b7.chunk.css"
+  },
+  {
+    "revision": "786b254f13638e4672e8",
+    "url": "/trello-style-app/static/js/2.e9014bb2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/trello-style-app/static/js/2.e9014bb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a27ceb1f73f865f90f8a",
+    "url": "/trello-style-app/static/js/main.eeb9f5b7.chunk.js"
+  },
+  {
+    "revision": "b935a440e55e1e156cf2",
+    "url": "/trello-style-app/static/js/runtime-main.e9622cdb.js"
+  }
+]);
