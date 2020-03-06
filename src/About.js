47bc5c39f94da89e9f1e43f5032965faf43f01de
AsaNeedle <asa@cottageclass.com>
2020-03-06 18:46:11 -0500

    created router

---
+++ src/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About(){
+  return (
+    <div className="container">
+      <header className="display-1" id="title" >Hacker Dreams</header>
+      <p className="lead">Don't let your dreams be dreams, hackers.</p>
+      <div>This is an app I made to practice art!</div>
+   </div>)
+}
+
+export default About;
