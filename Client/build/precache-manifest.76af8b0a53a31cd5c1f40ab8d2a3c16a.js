2329edb4b7ef20cc1ce9e27118e335d6d603fb53
AsaNeedle <asa@cottageclass.com>
2020-03-11 17:17:47 -0400

    created build file and pointed firebase to use it

---
+++ Client/build/precache-manifest.76af8b0a53a31cd5c1f40ab8d2a3c16a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea32edd030e0612509edb60d1c4bd08d",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "7ad683402d63d6bfec99",
+    "url": "/home/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "d6c6d3bd38fa8fac2376",
+    "url": "/home/static/css/main.cb9df8b7.chunk.css"
+  },
+  {
+    "revision": "7ad683402d63d6bfec99",
+    "url": "/home/static/js/2.9adc6bf6.chunk.js"
+  },
+  {
+    "revision": "0bc5220d92bfe34c3dec2f124a7bb286",
+    "url": "/home/static/js/2.9adc6bf6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6c6d3bd38fa8fac2376",
+    "url": "/home/static/js/main.9541863e.chunk.js"
+  },
+  {
+    "revision": "97e20a68d71b7a65ea58",
+    "url": "/home/static/js/runtime-main.e7ee9056.js"
+  }
+]);
